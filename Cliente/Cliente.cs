a9942d2eec4c72c27dfd3168bc457d2256aabf18
Jeffry Arias <jeffryariasp@gmail.com>
2020-10-18 23:04:33 -0600

    First Commit

---
+++ Cliente/Cliente.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Cliente : Form
+    {
+        public static ConexionServidor conexionServidor = new ConexionServidor();
+        public static String IP = "192.168.0.9";
+        public const int puerto = 25565;
+        public string usuario;
+        public Cliente()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+
+        #region Eventos
+        //Botón que inicia sesión
+        private void btnInicioSesion_Click(object sender, EventArgs e)
+        {
+
+            if (!conexionServidor.Conectar(IP, puerto))
+            {
+                MessageBox.Show("El servidor no se encuentra disponible, verifique que se encuentre abierto.", "Se ha presentado un error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                usuario = txtUsuario.Text;
+                var mensaje = new DatosServidor("login", txtUsuario.Text + "," + txtPsswd.Text);
+                conexionServidor.EnviarPaquete(mensaje);
+            }
+        }
+        //Botón salir
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Cliente_Load(object sender, EventArgs e)
+        {
+            conexionServidor.OnDataRecieved += MensajeRecibido;
+        }
+
+        private void Cliente_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        #endregion
+
+        #region Procedimientos
+        //Método que recibe los mensajes del servidor
+        private void MensajeRecibido(string datos)
+        {
+            var datosServidor = new DatosServidor(datos);
+            string comando = datosServidor.Accion;
+            ;
+            string contenido = datosServidor.Datos;
+            List<string> valores = DatosServidor.DeserializarLista(contenido);
+
+            //Mensaje para permitir entrar al usuario
+            if (comando.Equals("Ingresar"))
+            {
+                MessageBox.Show("Se ha iniciado sesión en el servidor", "Inicio de Sesion Correcto");
+                //btnInicioSesion.Enabled = false;
+            }
+
+            if (comando.Equals("No Ingresar"))
+            {
+                MessageBox.Show("El servidor ha rechazado su sesion", "Inicio de Sesion Prohibido");
+            }
+            //Mensaje que contiene las notas del textBox del servidor
+            if (comando.Equals("Nueva Notificacion"))
+            {
+                String notasServidor = string.Join(",", valores);
+                MessageBox.Show(notasServidor, "Notif. del servidor");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Cliente/ConexionServidor/DatosServidor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    public class DatosServidor
+    {
+
+        public string Accion { get; set; }
+        public string Datos { get; set; }
+
+        public DatosServidor()
+        {
+
+        }
+
+        public DatosServidor(string accion, string datos)
+        {
+            Accion = accion;
+            Datos = datos;
+        }
+
+        public DatosServidor(string datos)
+        {
+            int sepIndex = datos.IndexOf(":", StringComparison.Ordinal);
+            Accion = datos.Substring(0, sepIndex);
+            Datos = datos.Substring(Accion.Length + 1);
+        }
+
+        public string Serializar()
+        {
+            return string.Format("{0}:{1}", Accion, Datos);
+        }
+
+        public static implicit operator string(DatosServidor datos)
+        {
+            return datos.Serializar();
+        }
+
+        //Acciones a realizar con la lista
+
+        public static string SerializarLista(List<string> lista)
+        {
+            if (lista.Count == 0)
+            {
+                return null;
+            }
+
+            bool esElPrimero = true;
+            var salida = new StringBuilder();
+
+            foreach (var dato in lista)
+            {
+                if (esElPrimero)
+                {
+                    salida.Append(dato);
+                    esElPrimero = false;
+                }
+                else
+                {
+                    salida.Append(string.Format(",{0}", dato));
+                }
+            }
+            return salida.ToString();
+        }
+
+        public static List<string> DeserializarLista(string entrada)
+        {
+            string str = entrada;
+            var lista = new List<string>();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return lista;
+            }
+
+            try
+            {
+                foreach (string dato in entrada.Split(','))
+                {
+                    lista.Add(dato);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return lista;
+        }
+
+
+    }
+}
